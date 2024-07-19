bd482c338a0a6309b3b6bf19df34d68b38b138ea
Daniel <dkhoshkhoo3@gatech.edu>
2024-07-19 16:12:05 -0400

    Added speech to text

--- translation-app/src/components/MessageForm.js
+++ translation-app/src/components/MessageForm.js
@@ -1,26 +1,49 @@
-import { useState } from 'react';
-import { SendOutlined, PictureOutlined } from '@ant-design/icons';
+import { useEffect, useState } from 'react';
+import { SendOutlined, PictureOutlined, AudioOutlined, AudioFilled } from '@ant-design/icons';
 import { sendMessage, isTyping } from 'react-chat-engine';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
 
 const MessageForm = (props) => {
   const [value, setValue] = useState('');
   const { chatId, creds } = props;
 
+  const language = 'ru';
+
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition
+  } = useSpeechRecognition();
+
+  useEffect(() => {
+    setValue(transcript)
+  }, [transcript]);
+
+  const handleVoice = () => {
+    if (listening) {
+      SpeechRecognition.stopListening();
+      setValue(transcript);
+    } else {
+      SpeechRecognition.startListening({ language: language });
+    }
+  }
+
   const handleChange = (event) => {
     setValue(event.target.value);
-
     isTyping(props, chatId);
   };
 
   const handleSubmit = (event) => {
+    console.log("here")
+    SpeechRecognition.stopListening();
+    resetTranscript();
+
     event.preventDefault();
-
     const text = value.trim();
-
     if (text.length > 0) {
       sendMessage(creds, chatId, { text });
     }
-
     setValue('');
   };
 
@@ -49,6 +72,13 @@ const MessageForm = (props) => {
         style={{ display: 'none' }}
         onChange={handleUpload.bind(this)}
       />
+      {browserSupportsSpeechRecognition &&
+        <button type="button" onClick={handleVoice} className="send-button">
+          {
+            listening ? <AudioFilled className="speech-icon" /> : <AudioOutlined className="speech-icon" />
+          }
+        </button>
+      }
       <button type="submit" className="send-button">
         <SendOutlined className="send-icon" />
       </button>
